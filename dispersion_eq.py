0358dbe96b647c6e83ab3e6ef6ab82ac07f11a82
Breno Santos Cabral <59875400+BrenoSCabral@users.noreply.github.com>
2022-07-30 16:50:07 -0300

    Update dispersion_eq.py

--- dispersion_eq.py
+++ dispersion_eq.py
@@ -15,7 +15,9 @@ omega =  2*np.pi/T # angular frequency
 def dispersion_eq(k, w2 = omega**2, d = d, g = 9.8):
   return((g*k*np.tanh(d*k)/w2)-1) # -> written in a way that it should return 0, if well adjusted
 
-k = root(dispersion_eq, 1).x[0]
+o = 1 # order of magnitude of k
+
+k = root(dispersion_eq, o).x[0]
 L = 2 * np.pi/k # wavelength
 
 # dispersion_eq(k)  -> if you wish to see the proof
