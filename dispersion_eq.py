11de31b171fe41e28d29d816dd955532b536c977
Breno Santos Cabral <59875400+BrenoSCabral@users.noreply.github.com>
2022-07-30 16:20:58 -0300

    Adding script

---
+++ dispersion_eq.py
@@ -0,0 +1,21 @@
+##################################
+### CREATED BY: BRENO CABRAL ####
+########## JULY/2022 ############
+import numpy as np
+from scipy.optimize import root
+
+def dispersion_eq(k, w2 = omega**2, d = d, g = 9.8):
+  return((g*k*np.tanh(d*k)/w2)-1) # -> written in a way that it should return 0, if well adjusted
+
+
+# set your variables below:
+T = 8 # period
+d = 5 # depth
+
+omega =  2*np.pi/T # angular frequency
+
+
+k = root(dispersion_eq, 1).x[0]
+L = 2 * np.pi/k # wavelength
+
+# dispersion_eq(k)  -> if you wish to see the proof
